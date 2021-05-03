bda597107de08b37fab7ad567be8d70ed8cbc9e9
Charles <charles442009@gmail.com>
2021-05-03 00:58:37 -0700

    clarity to game over screen

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -8,16 +8,29 @@ class GameOver extends Phaser.Scene {
     }
 
     create() {
+        let menuConfig =
+        {
+            fontFamily: 'Times',
+            fontSize: '32px',
+            backgroundColor: '#4B0082',
+            color: '#ADD8E6',
+            align: 'right',
+            padding:
+            {
+                top: 10,
+                bottom: 10,
+            },
+            fixedWidth: 0
+        }
         this.map_1 = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'map_1').setOrigin(0, 0);
-        this.add.text(game.config.width / 2, game.config.height / 2, 'GAMEOVERRRRR').setOrigin(0.5);
-        this.add.text(game.config.width / 2, game.config.height / 2 + 64, 'Press (R) to Restart or ← to Menu').setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2, 'GAME OVERRRRR',menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2 + 64, 'Press (R) to Restart or ← to Menu',menuConfig).setOrigin(0.5);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
     }
-    
+
     update() {
         if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
             this.scene.start("menuScene")
         }
     }
  }
-  
